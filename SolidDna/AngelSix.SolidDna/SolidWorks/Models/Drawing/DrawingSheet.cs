1e3b698ac14d9105e4ff448a05b011f4b8ab5ad3
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2020-04-27 11:50:28 +0200

    Added class DrawingSheet and ModelEntity
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

---
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Models/Drawing/DrawingSheet.cs
@@ -0,0 +1,37 @@
+﻿using SolidWorks.Interop.swdocumentmgr;
+
+namespace AngelSix.SolidDna
+{
+    public class DrawingSheet : SolidDnaObject<SwDMSheet2>
+    {
+        #region Protected Members
+
+
+        #endregion
+
+        #region Public Properties
+
+        public string Name => BaseObject.Name;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DrawingSheet(SwDMSheet2 model) : base(model)
+        {
+        }
+
+        #endregion
+
+        #region Sheet Methods
+
+        #endregion
+
+        #region View Methods
+
+        #endregion
+    }
+}
---
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Models/Entity/ModelEntity.cs
@@ -0,0 +1,30 @@
+﻿using SolidWorks.Interop.sldworks;
+
+namespace AngelSix.SolidDna
+{
+    public class ModelEntity : SharedSolidDnaObject<Entity>
+    {
+        #region Protected Members
+
+        #endregion
+
+        #region Public Properties
+
+        public Component Component => new Component(BaseObject.GetComponent() as Component2);
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ModelEntity(Feature model) : base(model as Entity)
+        {
+        }
+
+        #endregion
+
+
+    }
+}
