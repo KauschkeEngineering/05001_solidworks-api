bbfc537b88cababb1d69347d763efe2aeafcf59a
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2020-04-27 11:19:03 +0200

    Added enums representing the solidworks SuppressionStates and SuppressionErrors Changed the getter of Name Property to return name without instance count Added Property NameWithInstanceCount, FilePath, SuppressionState, IsSuppressed, IsConsiderSuppressedHidden, IsHidden, IsLoaded. Added Method SetSupressionState, GetComponent,
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

--- SolidDna/AngelSix.SolidDna/SolidWorks/Models/Component/Component.cs
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Models/Component/Component.cs
@@ -1,4 +1,6 @@
 ﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.Linq;
 
 namespace AngelSix.SolidDna
 {
@@ -7,12 +9,30 @@ namespace AngelSix.SolidDna
     /// </summary>
     public class Component : SolidDnaObject<Component2>
     {
+        public enum SuppressionStates
+        {
+            Suppressed = swComponentSuppressionState_e.swComponentSuppressed,
+            Lightweight = swComponentSuppressionState_e.swComponentLightweight,
+            FullyResolved = swComponentSuppressionState_e.swComponentFullyResolved,
+            Resolved = swComponentSuppressionState_e.swComponentResolved,
+            FullyLightweight = swComponentSuppressionState_e.swComponentFullyLightweight,
+            InternalIdMismatch = swComponentSuppressionState_e.swComponentInternalIdMismatch
+        }
+
+        public enum SuppressionErrors
+        {
+            BadComponent = swSuppressionError_e.swSuppressionBadComponent,
+            BadState = swSuppressionError_e.swSuppressionBadState,
+            ChangeOk = swSuppressionError_e.swSuppressionChangeOk,
+            ChangeFailed = swSuppressionError_e.swSuppressionChangeFailed
+        }
+
         #region Public Properties
 
         /// <summary>
         /// Get the Model from the component
         /// </summary>
-        public Model AsModel => new Model (BaseObject.GetModelDoc2() as ModelDoc2);
+        public Model AsModel => new Model(BaseObject.GetModelDoc2() as ModelDoc2);
 
         /// <summary>
         /// Check if the Component is Root
@@ -25,9 +45,44 @@ namespace AngelSix.SolidDna
         public object[] Children => BaseObject.GetChildren() as object[];
 
         /// <summary>
-        /// Get the name of the component
+        /// Get the name of the component whithout the instance count number at the end identicated by -xx e.g. -1
         /// </summary>
-        public string Name => BaseObject.Name2;
+        public string Name
+        {
+            get
+            {
+                if (NameWithInstanceCount.Split('-').Length > 1)
+                    return NameWithInstanceCount.Remove(NameWithInstanceCount.Length - (NameWithInstanceCount.Split('-').Last().Length + 1));
+                else
+                    return NameWithInstanceCount;
+            }
+        }
+
+        public string NameWithInstanceCount => BaseObject.Name2;
+
+        public string FilePath => BaseObject.GetPathName();
+
+        public SuppressionStates SuppressionState => (SuppressionStates)BaseObject.GetSuppression();
+
+        public bool IsSuppressed => BaseObject.IsSuppressed();
+
+        // states vary depending on suppression and hidden state
+        // ConsiderSuppressed        Component       Component       IsHidden
+        //       True                 Hidden       Unsuppressed        True
+        //       True                 Hidden        Suppressed         True
+        //       True                 Shown        Unsuppressed        False
+        //       True                 Shown         Suppressed         True
+        public bool IsConsiderSuppressedHidden => BaseObject.IsHidden(true);
+
+        // states depending on hidden state
+        // ConsiderSuppressed        Component       Component       IsHidden
+        //       False                 Hidden       Unsuppressed        True
+        //       False                 Hidden        Suppressed         True
+        //       False                 Shown        Unsuppressed        False
+        //       False                 Shown         Suppressed         False
+        public bool IsHidden => BaseObject.IsHidden(false);
+
+        public bool IsLoaded => BaseObject.IsLoaded();
 
         #endregion
 
@@ -38,10 +93,19 @@ namespace AngelSix.SolidDna
         /// </summary>
         public Component(Component2 component) : base(component)
         {
+        }
 
+        public SuppressionErrors SetSupressionState(SuppressionStates suppressionState)
+        {
+            return (SuppressionErrors)BaseObject.SetSuppression2((int)suppressionState);
         }
 
         #endregion
+
+        public static Component GetComponent(object component)
+        {
+            return new Component((Component2)component);
+        }
 
         #region Dispose
 
