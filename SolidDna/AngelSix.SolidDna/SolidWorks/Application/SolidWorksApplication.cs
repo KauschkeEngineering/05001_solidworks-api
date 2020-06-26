23fcacb1853c549558f2a5f8af47f9eaf3b733ac
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2020-06-26 17:16:49 +0200

    -
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

--- SolidDna/AngelSix.SolidDna/SolidWorks/Application/SolidWorksApplication.cs
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Application/SolidWorksApplication.cs
@@ -962,7 +962,6 @@ namespace AngelSix.SolidDna
                 var dummyModel = new Model((ModelDoc2)BaseObject.NewDocument(templateFilePath, 0, 0, 0));
                 BaseObject.DocumentVisible(false, nDocType);
 
-                //dummyModel.SetUserControlable(false, false, false, true);
                 return dummyModel;
             }
             return null;
