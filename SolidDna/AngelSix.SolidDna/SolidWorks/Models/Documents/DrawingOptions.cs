7e4f11b48eff6a5117bbefb48e4e90fd83d78185
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2020-09-11 17:27:57 +0200

    Added comments. Changed enum to obtaine values of swBackgroundProcessOption_e enum.
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

--- SolidDna/AngelSix.SolidDna/SolidWorks/Models/Documents/DrawingOptions.cs
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Models/Documents/DrawingOptions.cs
@@ -1,10 +1,19 @@
-﻿namespace AngelSix.SolidDna
+﻿using SolidWorks.Interop.swconst;
+
+namespace AngelSix.SolidDna
 {
+    /// <summary>
+    /// Background processing options.
+    /// <see cref="swBackgroundProcessOption_e"/>
+    /// </summary>
     public enum BackgroundProcessOptions
     {
-        BackgroundProcessingDisabled = 0,
-        BackgroundProcessingEnabled,
-        BackgroundProcessingDeferToApplication
+        // disabled
+        BackgroundProcessingDisabled = swBackgroundProcessOption_e.swBackgroundProcessing_Disabled,
+        // enabled
+        BackgroundProcessingEnabled = swBackgroundProcessOption_e.swBackgroundProcessing_Enabled,
+        // defer to ISldWorks::EnableBackgroundProcessing setting
+        BackgroundProcessingDeferToApplication = swBackgroundProcessOption_e.swBackgroundProcessing_DeferToApplication
     }
 
 }
