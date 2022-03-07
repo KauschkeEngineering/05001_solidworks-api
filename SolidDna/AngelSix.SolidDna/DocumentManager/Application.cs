7f81dba0654561876cb5cedc9392df7a615a3cd3
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2022-03-07 09:27:15 +0100

    Added new class MaterialDatabase. Added singelton class for application document manager.
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

---
+++ SolidDna/AngelSix.SolidDna/DocumentManager/Application.cs
@@ -0,0 +1,91 @@
+﻿using SolidWorks.Interop.swdocumentmgr;
+using static AngelSix.SolidDna.Model;
+
+namespace AngelSix.SolidDna.DocumentManager
+{
+    public sealed class Application
+    {
+        private static Application _instance = null;
+        private static readonly SwDMClassFactory _classFactory = new SwDMClassFactory();
+        private readonly SwDMApplication _documentManagerApplication;
+
+
+        private Application(string licKey)
+        {
+            _documentManagerApplication = _classFactory.GetApplication(licKey);
+        }
+
+        public static Application GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Application(Credential.getSolidWorksLicenseAPIKey());
+            }
+            return _instance;
+        }
+
+        public SwDmDocumentType GetDocumentType(string filePath)
+        {
+            // Determine type of SOLIDWORKS file based on file extension
+            if (filePath.ToLower().EndsWith(PartDocument.FILE_EXTENSION))
+            {
+                return SwDmDocumentType.swDmDocumentPart;
+            }
+            else if (filePath.ToLower().EndsWith(AssemblyDocument.FILE_EXTENSION))
+            {
+                return SwDmDocumentType.swDmDocumentAssembly;
+            }
+            else if (filePath.ToLower().EndsWith(DrawingDocument.FILE_EXTENSION))
+            {
+                return SwDmDocumentType.swDmDocumentDrawing;
+            }
+            else
+            {
+                return SwDmDocumentType.swDmDocumentUnknown;
+            }
+        }
+
+        public MajorSolidWorksVersions GetFileSWVersion(string filePath)
+        {
+
+            if (_instance._documentManagerApplication != null)
+            {
+                var _swDoc = _instance._documentManagerApplication.GetDocument(filePath, GetDocumentType(filePath), true, out var _retVal);
+                if (_swDoc != null)
+                {
+                    return (MajorSolidWorksVersions)_swDoc.GetVersion();
+                }
+            }
+            return MajorSolidWorksVersions.UNKNOWN;
+        }
+
+        public string[] GetDocumentReferences(string documentFileName)
+        {
+            int _docType;
+            switch (GetDocumentType(documentFileName))
+            {
+                case SwDmDocumentType.swDmDocumentUnknown:
+                    return null;
+                case SwDmDocumentType.swDmDocumentPart:
+                    _docType = (int)SwDmDocumentType.swDmDocumentPart;
+                    break;
+                case SwDmDocumentType.swDmDocumentAssembly:
+                    _docType = (int)SwDmDocumentType.swDmDocumentAssembly;
+                    break;
+                case SwDmDocumentType.swDmDocumentDrawing:
+                    _docType = (int)SwDmDocumentType.swDmDocumentDrawing;
+                    break;
+                default:
+                    return null;
+            }
+
+            var swSearchOpt = _documentManagerApplication.GetSearchOptionObject();
+            SwDmDocumentOpenError nRetVal;
+            var swDoc = (SwDMDocumentClass)_documentManagerApplication.GetDocument(documentFileName, (SwDmDocumentType)_docType, true, out nRetVal);
+            if (swDoc != null && nRetVal == SwDmDocumentOpenError.swDmDocumentOpenErrorNone)
+                return (string[])swDoc.GetAllExternalReferences(swSearchOpt);
+            return null;
+        }
+
+    }
+}
---
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Application/Material/MaterialDatabase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngelSix.SolidDna
+{
+    public class MaterialDatabase
+    {
+
+        #region Constants
+
+        public const string DEFAULT_EXTENSION = ".sldmat";
+        public const string FILE_FILTER = " (" + DEFAULT_EXTENSION + ")|*" + DEFAULT_EXTENSION;
+
+        #endregion
+
+        #region Private Members
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get
+            {
+                if (!FullPath.Equals(""))
+                {
+                    var split = FullPath.Split('\\');
+                    if (split.Length > 0)
+                    {
+                        return split[split.Length - 1].Replace(DEFAULT_EXTENSION, "");
+                    }
+                    return FullPath;
+                }
+                return "";
+            }
+        }
+
+        public string FullPath { get; private set; }
+        public bool Found 
+        { 
+            get
+            {
+                if (Name.Equals("") || Name.Equals(FullPath))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public bool IsNone { get; } = false;
+
+        #endregion
+
+        #region Constructor
+
+        public MaterialDatabase(string fullPath)
+        {
+            FullPath = fullPath;
+            if (fullPath.Equals("Keine"))
+            {
+                IsNone = true;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+    }
+}
