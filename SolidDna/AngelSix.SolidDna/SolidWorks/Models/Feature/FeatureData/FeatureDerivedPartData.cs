50cc3613d4972e4a55afa7a0f38fd2ff8674f854
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2023-03-28 19:44:59 +0200

    Added property PathName. Modified constructor. Added method for getting model. Added method to access selections. Added method for releasing selection access.
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

--- SolidDna/AngelSix.SolidDna/SolidWorks/Models/Feature/FeatureData/FeatureDerivedPartData.cs
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Models/Feature/FeatureData/FeatureDerivedPartData.cs
@@ -1,4 +1,6 @@
 ﻿using SolidWorks.Interop.sldworks;
+using System.IO;
+using System.Web.WebSockets;
 
 namespace AngelSix.SolidDna
 {
@@ -7,16 +9,39 @@ namespace AngelSix.SolidDna
     /// </summary>
     public class FeatureDerivedPartData : SolidDnaObject<IDerivedPartFeatureData>
     {
+        public string PathName => BaseObject.PathName;
+
         #region Constructor
 
         /// <summary>
         /// Default constructor
         /// </summary>
-        public FeatureDerivedPartData(IDerivedPartFeatureData model) : base(model)
+        public FeatureDerivedPartData(object model) : base((IDerivedPartFeatureData)model)
         {
 
         }
 
         #endregion
+
+        public Model GetModel(Model TopLevelModel)
+        {
+            //AddInIntegration.SolidWorks.ActivateDocument(new FileInfo(PathName).Name);
+            //var model = BaseObject.GetModelDoc();
+            //var Model1 = new AngelSix.SolidDna.Model(model);
+            return AddInIntegration.SolidWorks.OpenFile(PathName);
+            
+        }
+
+        private bool AccessSelections(Model TopLevelModel, Component componentToModify = null)
+        {
+            var status = BaseObject.AccessSelections(TopLevelModel.AsModelDoc2, componentToModify);
+            return status;
+        }
+
+        private void ReleaseSelectionAccess()
+        {
+            BaseObject.ReleaseSelectionAccess();
+        }
+
     }
 }
