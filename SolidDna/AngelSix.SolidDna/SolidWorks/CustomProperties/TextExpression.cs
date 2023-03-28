68b7fd93370ee2356433c95552b2b0c4f35672b8
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2023-03-28 19:49:36 +0200

    Added class TextExpression. Added custom_property_de.texpr containing german text expressions. Added custom_property_en.texpr containing english text expressions.
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

---
+++ SolidDna/AngelSix.SolidDna/SolidWorks/CustomProperties/TextExpression.cs
@@ -0,0 +1,57 @@
+﻿using static AngelSix.SolidDna.Model;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace AngelSix.SolidDna.SolidWorks.CustomProperties
+{
+    public class TextExpression
+    {
+
+        #region Constants
+
+        public const string DEFAULT_EXTENSION = ".texpr";
+
+        #endregion
+
+        #region Private Members
+
+        private string _name = string.Empty;
+        private string _expression = string.Empty;
+
+        #endregion
+
+        #region Properties
+
+        [XmlElement("Name")]
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        [XmlElement("Expression")]
+        public string Expression
+        {
+            get => _expression;
+            set => _expression = value;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public TextExpression(string name, string expression)
+        {
+            _name = name;
+            _expression = expression;
+        }
+
+        public TextExpression()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
