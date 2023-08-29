35ad5f77a23c5e6d2401f64161f1db353ca6bb55
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2023-08-29 17:49:26 +0200

    Added method DisposeModelFeature.
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

--- SolidDna/AngelSix.SolidDna/SolidWorks/Models/Feature/Base/ModelFeature.cs
+++ SolidDna/AngelSix.SolidDna/SolidWorks/Models/Feature/Base/ModelFeature.cs
@@ -1124,6 +1124,11 @@ namespace AngelSix.SolidDna
 
         #region Dispose
 
+        public void DisposeModelFeature()
+        {
+            Dispose();
+        }
+
         /// <summary>
         /// Disposing
         /// </summary>
